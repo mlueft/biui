f85f78774c33a617b0dcc406914891ffc5aa712e
Michael Lüftenegger <office@lueftenegger.at>
2022-11-28 08:14:33 +0100

    Basic mouse controladded.

--- src/biui/Mouse.py
+++ src/biui/Mouse.py
@@ -1,10 +1,38 @@
 import ctypes
+import sdl2
+from sdl2.events import SDL_DISABLE, SDL_ENABLE
 
 class Mouse:
     
     def __init__(self):
-        
+        pass
+    
     ### Returns the mouse position on screen.
     ##
     ##
-    def position():
+    def getPosition():
+        x = ctypes.c_int()
+        y = ctypes.c_int()
+        sdl2.SDL_GetGlobalMouseState(ctypes.byref(x),ctypes.byref(y));
+        return (x.value,y.value)
+    
+    ###
+    ##
+    ##
+    def setPosition(x,y):
+        sdl2.SDL_WarpMouseGlobal(x,y)
+
+    ###
+    ##
+    ##
+    def hide():
+        sdl2.SDL_ShowCursor(SDL_DISABLE)
+    ###
+    ##
+    ##
+    def show():
+        sdl2.SDL_ShowCursor(SDL_ENABLE)
+        
+        
+        
+        
